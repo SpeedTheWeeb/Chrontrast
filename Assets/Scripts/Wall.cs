5fe04f8f935e01c362b23e3228f0ed6582dbd0ed
99Kajkager <42999933+99Kajkager@users.noreply.github.com>
2021-10-05 13:21:40 +0200

    Update Wall.cs
    Fixed syntax errors

--- Assets/Scripts/Wall.cs
+++ Assets/Scripts/Wall.cs
@@ -3,41 +3,40 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class NewBehaviourScript : MonoBehaviour
-
-// Hits needed to break
-public int hitsNeeded = 3;
-
-// Value used to store hits taken in order to break
-public int hitsTaken;
-
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+    // Hits needed to break
+    public int hitsNeeded = 3;
 
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
+    // Value used to store hits taken in order to break
+    public int hitsTaken;
 
-    /* 
-        This "OnCollisionEnter2D" checks for what I assume to be any
-        2D collision. This needs to be changed in the future to check
-        for a specific colision made by certain objects (attacks from
-        enemies in the game). 
-        Long story short, I don't know how this check is made in Unity
-        (sorry). Also these objects don't exist, so I wouldn't know the
-        name of the objects I'm checking for anyways.
-    */ 
-    void OnCollisionEnter2D(Collision2D collisionInfo)
-    {
-        hitsTaken += 1;
+        // Start is called before the first frame update
+        void Start()
+        {
         
-        if (hitsTaken >= hitsNeeded) {
-            Destroy (gameObject);
         }
-    }
+
+        // Update is called once per frame
+        void Update()
+        {
+        
+        }
+
+        /* 
+            This "OnCollisionEnter2D" checks for what I assume to be any
+            2D collision. This needs to be changed in the future to check
+            for a specific colision made by certain objects (attacks from
+            enemies in the game). 
+            Long story short, I don't know how this check is made in Unity
+            (sorry). Also these objects don't exist, so I wouldn't know the
+            name of the objects I'm checking for anyways.
+        */ 
+        void OnCollisionEnter2D(Collision2D collisionInfo)
+        {
+            hitsTaken += 1;
+        
+            if (hitsTaken >= hitsNeeded) {
+                Destroy (gameObject);
+            }
+        }
 }
