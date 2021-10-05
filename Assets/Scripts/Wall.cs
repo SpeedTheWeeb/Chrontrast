48c496a7569c8916191a35f759ccf0aafc88a21b
99Kajkager <42999933+99Kajkager@users.noreply.github.com>
2021-10-05 13:23:14 +0200

    Update Wall.cs

--- Assets/Scripts/Wall.cs
+++ Assets/Scripts/Wall.cs
@@ -10,33 +10,34 @@ public class NewBehaviourScript : MonoBehaviour
     // Value used to store hits taken in order to break
     public int hitsTaken;
 
-        // Start is called before the first frame update
-        void Start()
-        {
+    // Start is called before the first frame update
+    void Start()
+    {
         
-        }
+    }
 
-        // Update is called once per frame
-        void Update()
-        {
+    // Update is called once per frame
+    void Update()
+    {
         
-        }
+    }
 
-        /* 
-            This "OnCollisionEnter2D" checks for what I assume to be any
-            2D collision. This needs to be changed in the future to check
-            for a specific colision made by certain objects (attacks from
-            enemies in the game). 
-            Long story short, I don't know how this check is made in Unity
-            (sorry). Also these objects don't exist, so I wouldn't know the
-            name of the objects I'm checking for anyways.
-        */ 
-        void OnCollisionEnter2D(Collision2D collisionInfo)
-        {
-            hitsTaken += 1;
+    /* 
+        This "OnCollisionEnter2D" checks for what I assume to be any
+        2D collision. This needs to be changed in the future to check
+        for a specific colision made by certain objects (attacks from
+        enemies in the game). 
+        Long story short, I don't know how this check is made in Unity
+        (sorry). Also these objects don't exist, so I wouldn't know the
+        name of the objects I'm checking for anyways.
+    */ 
+
+    void OnCollisionEnter2D(Collision2D collisionInfo)
+    {
+        hitsTaken += 1;
         
-            if (hitsTaken >= hitsNeeded) {
-                Destroy (gameObject);
-            }
+        if (hitsTaken >= hitsNeeded) {
+            Destroy (gameObject);
         }
+    }
 }
