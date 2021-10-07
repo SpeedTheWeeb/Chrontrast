aa242d39649d543d34c025c6c75058aa5935fa65
99Kajkager <42999933+99Kajkager@users.noreply.github.com>
2021-10-07 10:30:54 +0200

    Update Wall.cs
    added "if (collisionInfo.collider.tag == "projectile")" following the function in the CrystalHP.cs-file

--- Assets/Scripts/Wall.cs
+++ Assets/Scripts/Wall.cs
@@ -22,22 +22,15 @@ public class NewBehaviourScript : MonoBehaviour
         
     }
 
-    /* 
-        This "OnCollisionEnter2D" checks for what I assume to be any
-        2D collision. This needs to be changed in the future to check
-        for a specific colision made by certain objects (attacks from
-        enemies in the game). 
-        Long story short, I don't know how this check is made in Unity
-        (sorry). Also these objects don't exist, so I wouldn't know the
-        name of the objects I'm checking for anyways.
-    */ 
-
     void OnCollisionEnter2D(Collision2D collisionInfo)
     {
-        hitsTaken += 1;
+        if (collisionInfo.collider.tag == "projectile") //Skal ændre til entity hvis vi vælger dette system
+        {
+            hitsTaken += 1;
         
-        if (hitsTaken >= hitsNeeded) {
+            if (hitsTaken >= hitsNeeded) {
             Destroy (gameObject);
+            }
         }
     }
 }
