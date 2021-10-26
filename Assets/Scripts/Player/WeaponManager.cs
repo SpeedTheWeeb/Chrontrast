1366b469a3608d0efb8c29541c6aec1768ab52ee
EmilStorm <90619317+EmilStorm@users.noreply.github.com>
2021-10-26 12:40:15 +0200

    Splash Weapon (Kun Sweetspot so far) & powerup prefabs

--- Assets/Scripts/Player/WeaponManager.cs
+++ Assets/Scripts/Player/WeaponManager.cs
@@ -5,7 +5,7 @@ using UnityEngine;
 public class WeaponManager : MonoBehaviour
 {
     public bool alreadyHolding = false;    
-    public int weaponType = 4;
+    public int weaponType = 0;
     public int playerNumber = 2;
 
     public Transform meleeHurtbox;
@@ -14,6 +14,15 @@ public class WeaponManager : MonoBehaviour
     public float meleeSpeed = 2f;       // Adjusts time before next attack is ready
     float nextMeleeAttackTime = 0f;
     // public Animator meleeAnim;
+
+    // public Transform splashSourSpot;
+    public Transform splashSweetSpot;
+    public float sweetSpotRadius = 1f;
+    // public float sourSpotDamage = 5f;
+    public float sweetSpotDamage = 15f;
+    public float splashSpeed = 2f;       // Adjusts time before next attack is ready
+    float nextSplashAttackTime = 0f;
+    // public Animator splashAnim;
 
     private void Update()
     {
@@ -26,17 +35,18 @@ public class WeaponManager : MonoBehaviour
                     if (Input.GetButtonDown("Fire" + playerNumber) && alreadyHolding)
                     {
                         MeleeAttack();
-                        nextMeleeAttackTime = Time.time + 1f / meleeSpeed; // Counts how many seconds until next attack is ready
+                        nextMeleeAttackTime = Time.time + 1f / meleeSpeed; // Counts how many times pr second you can swing.
                     }
                 }
                 break;
 
             case 2:
                 Debug.Log("I am holding a Splash Weapon!");
-                /*if (Input.GetButtonDown("Fire" + playerNumber) && alreadyHolding)
+                if (Input.GetButtonDown("Fire" + playerNumber) && alreadyHolding)
                 {
-                    SplashAttack();
-                }*/
+                    SweetSpotSplashAttack();
+                    nextSplashAttackTime = Time.time + 1f / splashSpeed;
+                }
                 break;
 
             case 3:
@@ -51,6 +61,13 @@ public class WeaponManager : MonoBehaviour
                 Debug.Log("I am not holding any Weapon!");
                 break;
         }
+
+        /*if(Input.GetButtonDown("Drop" + playerNumber) && alreadyHolding)
+        {
+            
+            weaponType = 0;
+            alreadyHolding = false;
+        }*/
     }
 
     private void MeleeAttack()
@@ -72,11 +89,34 @@ public class WeaponManager : MonoBehaviour
         }
     }
 
+    private void SweetSpotSplashAttack()
+    {
+        Debug.Log("I am shooting Bawls of Fiyah!");
+        // play animation
+        // Animator.SetTrigger("MeleeAttack");
+
+        // Detects enemies in range
+        Collider2D[] sweetSpotEnemies = Physics2D.OverlapCircleAll(splashSweetSpot.position, sweetSpotRadius);
+
+        // Applies damage
+        foreach (Collider2D enemy in sweetSpotEnemies)
+        {
+            if (enemy.CompareTag("Enemy"))
+            {
+                enemy.GetComponent<EnemyHealth>().TakeDamage(sweetSpotDamage);
+            }
+        }
+    }   
+
     private void OnDrawGizmos()
     {
         if (meleeHurtbox == null)
             return;
-
         Gizmos.DrawWireSphere(meleeHurtbox.position, meleeRange);
+
+
+        if (splashSweetSpot == null)
+            return;
+        Gizmos.DrawWireSphere(splashSweetSpot.position, sweetSpotRadius);
     }
 }
--- Assets/Scripts/PowerUp/PowerUp.cs
+++ Assets/Scripts/PowerUp/PowerUp.cs
@@ -6,24 +6,24 @@ public class PowerUp : MonoBehaviour
 {
     private void OnTriggerEnter2D(Collider2D other)
     {
-        if (other.CompareTag("Player"))
+        if (other.CompareTag("Player") && other.GetComponent<WeaponManager>().alreadyHolding == false)
         {
             other.GetComponent<WeaponManager>().weaponType = 0;
             switch(gameObject.name)
             {
-                case "MeleePowerUp":
+                case "MeleePowerUp":                    
                     other.GetComponent<WeaponManager>().weaponType = 1;
-                    other.GetComponent<WeaponManager>().alreadyHolding = true;
+                    other.GetComponent<WeaponManager>().alreadyHolding = true;                    
                     break;
 
-                case "ShotgunPowerUp":
+                case "ShotgunPowerUp":                    
                     other.GetComponent<WeaponManager>().weaponType = 2;
-                    other.GetComponent<WeaponManager>().alreadyHolding = true;
+                    other.GetComponent<WeaponManager>().alreadyHolding = true;                    
                     break;
 
-                case "SniperPowerUp":
+                case "SniperPowerUp":                    
                     other.GetComponent<WeaponManager>().weaponType = 3;
-                    other.GetComponent<WeaponManager>().alreadyHolding = true;
+                    other.GetComponent<WeaponManager>().alreadyHolding = true;                    
                     break;
             }
             Pickup(other);
