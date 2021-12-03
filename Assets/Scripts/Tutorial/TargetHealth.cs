08359eb6b4ca141d03738a067798e0c3f1e16824
99Kajkager <42999933+99Kajkager@users.noreply.github.com>
2021-12-03 12:15:00 +0100

    Tutorial scene setup, Main Menu button for Tutorial now works, as well as pause menu adjustment in tutorial scene

---
+++ Assets/Scripts/Tutorial/TargetHealth.cs
@@ -0,0 +1,56 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+// Burde inherit fra EnemyHealth
+public class TargetHealth : MonoBehaviour
+{
+    public float maxHealth = 100f;
+    Text text;
+    SpawningScript Spawning;
+    public float currentHealth;
+    int modifier = 0;
+    Color ogColor;
+    public SpriteRenderer render;
+    private void Start()
+    {
+        GameObject crystal = GameObject.Find("Crystal");
+        SpawningScript wave = (SpawningScript)crystal.GetComponent("SpawningScript");
+        if(wave.currentWave % 2 == 0)
+        {
+            modifier += 10;
+        }
+        currentHealth = maxHealth + ((modifier/maxHealth)*100);
+        render = GetComponent<SpriteRenderer>();
+        ogColor = render.color;
+    }
+
+    public void TakeDamage(float damage)
+    {
+        
+        currentHealth -= damage;
+        Flash();
+        if (currentHealth <= 0)
+            Die();
+    }
+
+    void Flash()
+    {
+        Color col = new Color(255/255f, 125/255f, 125/255f);
+        render.color = col;
+        Invoke("ResetFlash", 0.3f);
+    }
+    private void ResetFlash()
+    {
+        render.color = ogColor;
+    }
+
+    void Die()
+    {
+        Debug.Log("Enemy died!");
+
+        Destroy(gameObject);
+    }
+    
+}
